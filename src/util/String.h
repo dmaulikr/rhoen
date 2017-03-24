9384accddafdb4e3a09962a393b164be746ce5fd
Leon Krieg <leonkrieg93@gmail.com>
2017-03-24 22:01:06 +0000

    Remove word for uniformity

--- src/util/String.h
+++ src/util/String.h
@@ -3,7 +3,7 @@ namespace ul {
 }
 
 /**
- * Container class for managing characters sequences.
+ * Container for managing characters sequences.
  * Keeps an internal buffer to store any sequence of characters. Will also
  * allocate more memory if the string length exceeds its buffer size. This
  * implementation should be pretty standard for a C++ string. New features
