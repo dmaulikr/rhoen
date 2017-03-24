7992837b84d499e6d66d5698cd55f193cf48d707
Leon Krieg <leonkrieg93@gmail.com>
2017-03-24 17:43:59 +0000

    Remove brief keyword from comment

--- src/util/String.h
+++ src/util/String.h
@@ -1,5 +1,5 @@
 /**
- * @brief Container class for managing characters sequences.
+ * Container class for managing characters sequences.
  * Keeps an internal buffer to store any sequence of characters. Will also
  * allocate more memory if the string length exceeds its buffer size. This
  * implementation should be pretty standard for a C++ string. New features
@@ -11,6 +11,6 @@ public:
 	String();
 	~String();
 
-	int Length() const; ///< Get length of string
-	int Cmp(const char *str) const; ///< Compare string with char array
+	int Length() const; ///< Gets length of string
+	int Cmp(const char *str) const; ///< Compares string with char array
 };
