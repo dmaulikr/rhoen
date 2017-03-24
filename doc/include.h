8531326e89a8996ac9ee3113233f05acfc281c08
Leon Krieg <leonkrieg93@gmail.com>
2017-03-24 18:51:53 +0000

    Add include.h for additional Doxygen definitions

---
+++ doc/include.h
@@ -0,0 +1,9 @@
+/** defgroup util Utility Library */
+/** defgroup engine Engine Framework */
+/** defgroup game Game */
+
+/** @namespace util
+ *  @ingroup util
+ *  @brief Library for containers, algorithms and all other low-level
+ *         functionality that is needed by the engine.
+ */
