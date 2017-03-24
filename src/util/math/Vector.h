bf7a2761d29055d668d40121f4b348015b2f2242
Leon Krieg <leonkrieg93@gmail.com>
2017-03-24 21:59:25 +0000

    Fix typos

--- src/util/math/Vector.h
+++ src/util/math/Vector.h
@@ -3,8 +3,8 @@ namespace ul {
 }
 
 /**
- * Container class for storing floating point values.
- * Can be used like regular arrays, and often describe a point or
+ * Container for storing floating point values.
+ * Can be used like an array and often describes a point or
  * distances in space. Mathematical operations can be performed
  * with vectors and matrices, which makes it possible to rotate
  * transform or translate the defined points in 3D perspective.
