6c6a0e68c9bd0c88fe31a9f6b230b64457ae998d
Leon Krieg <leonkrieg93@gmail.com>
2017-03-24 17:33:02 +0000

    Write brief description of string class

--- src/util/String.h
+++ src/util/String.h
@@ -1,6 +1,16 @@
+/**
+ * @brief Container class for managing characters sequences.
+ * Keeps an internal buffer to store any sequence of characters. Will also
+ * allocate more memory if the string length exceeds its buffer size. This
+ * implementation should be pretty standard for a C++ string. New features
+ * will be added when they are needed by other parts of the project.
+ */
 class String
 {
 public:
 	String();
 	~String();
+
+	int Length() const; ///< Get length of string
+	int Cmp(const char *str) const; ///< Compare string with char array
 };
