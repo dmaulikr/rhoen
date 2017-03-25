68c8f90356b9371f204427bb0921dd12695d6174
Leon Krieg <leonkrieg93@gmail.com>
2017-03-25 19:06:25 +0000

    Formatting, add TODO

--- test/core/testEngine.cpp
+++ test/core/testEngine.cpp
@@ -1,7 +1,9 @@
 #include <core.h>
 #include <catch.h>
 
-TEST_CASE("Core submodules can be initialized", "[Engine]") {
+TEST_CASE("Core submodules can be initialized", "[Engine]")
+{
+	// TODO
     REQUIRE(1 == 1);
     REQUIRE(2 == 2);
     REQUIRE(3 == 3);
