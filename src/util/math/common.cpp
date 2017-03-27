cf138a7df0691594868ad6dfb7ca9122b219dcfa
Leon Krieg <leonkrieg93@gmail.com>
2017-03-27 12:36:37 +0000

    Use sqrt function provided by math.h

--- src/util/math/common.cpp
+++ src/util/math/common.cpp
@@ -1,21 +1,10 @@
+#include <math.h>
+
 namespace util {
-
-float InvSqrt(float x)
-{
-	float xhalf;
-	int i;
-
-	xhalf = 0.5f * x;
-	i = *(int*)&x;              // store floating-point bits in integer
-	i = 0x5f3759df - (i >> 1);  // initial guess for Newton's method
-	x = *(float*)&i;            // convert new bits into float
-	x = x*(1.5f - xhalf*x*x);   // one round of Newton's method
-	return x;
-}
 
 float Sqrt(float x)
 {
-	return x * InvSqrt(x);
+	return ::sqrt(x);
 }
 
 } // namespace util
--- src/util/math/common.h
+++ src/util/math/common.h
@@ -1,13 +1,7 @@
 namespace util {
 
 /**
- * Fast inverse square root by Carmack/Mathisen/Tarolli.
- * There are better ways to do this nowadays but this is magic!
- */
-float InvSqrt(float x);
-
-/**
- * Calculate square root by inverting the inverse square root.
+ * Calculate square root.
  */
 float Sqrt(float x);
 
