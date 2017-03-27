46f7fd45f232511fc7b051b5bb8c0480003705bb
Leon Krieg <leonkrieg93@gmail.com>
2017-03-27 14:18:21 +0000

    Fix Vector2 compound subtraction operator

--- src/util/Vector.cpp
+++ src/util/Vector.cpp
@@ -86,7 +86,7 @@ Vector2 operator+(const Vector2 &a, const Vector2 &b)
 
 Vector2 operator-(const Vector2 &a, const Vector2 &b)
 {
-	return Vector2((a.x+b.x), (a.y+b.y));
+	return Vector2((a.x-b.x), (a.y-b.y));
 }
 
 } // namespace util
