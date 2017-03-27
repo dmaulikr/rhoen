99506ac94ed9f2d30a2c29fb77857d7457841b40
Leon Krieg <leonkrieg93@gmail.com>
2017-03-27 09:58:53 +0000

    Implement basic Vector2 class

--- src/util/math/Vector.cpp
+++ src/util/math/Vector.cpp
@@ -1,11 +1,91 @@
 #include <util.h>
-using namespace util;
+namespace util {
 
-
-Vector::Vector()
+Vector2::Vector2()
 {
 }
 
-Vector::~Vector()
+Vector2::~Vector2()
 {
 }
+
+Vector2::Vector2(const Vector2 &vec)
+{
+	x = vec.x;
+	y = vec.y;
+}
+
+Vector2::Vector2(const float vx, const float vy)
+{
+	x = vx;
+	y = vy;
+}
+
+void Vector2::Set(const float vx, const float vy)
+{
+	x = vx;
+	y = vy;
+}
+
+bool Vector2::Compare(const Vector2 &vec) const
+{
+	return ((x == vec.x) && (y == vec.y));
+}
+
+float Vector2::Length() const
+{
+	// TODO
+	return 0;
+}
+
+float Vector2::operator[](int index) const
+{
+	return (&x)[index];
+}
+
+float &Vector2::operator[](int index)
+{
+	return (&x)[index];
+}
+
+void Vector2::operator=(const Vector2 &vec)
+{
+	x = vec.x;
+	y = vec.y;
+}
+
+Vector2 &Vector2::operator+=(const Vector2 &vec)
+{
+	x += vec.x;
+	y += vec.y;
+	return *this;
+}
+
+Vector2 &Vector2::operator-=(const Vector2 &vec)
+{
+	x -= vec.x;
+	y -= vec.y;
+	return *this;
+}
+
+bool operator==(const Vector2 &a, const Vector2 &b)
+{
+	return a.Compare(b);
+}
+
+bool operator!=(const Vector2 &a, const Vector2 &b)
+{
+	return !(a.Compare(b));
+}
+
+Vector2 operator+(const Vector2 &a, const Vector2 &b)
+{
+	return Vector2((a.x+b.x), (a.y+b.y));
+}
+
+Vector2 operator-(const Vector2 &a, const Vector2 &b)
+{
+	return Vector2((a.x+b.x), (a.y+b.y));
+}
+
+} // namespace util
--- src/util/math/Vector.h
+++ src/util/math/Vector.h
@@ -1,18 +1,39 @@
 namespace util {
-	class Vector;
-}
 
 /**
  * Container for storing floating point values.
  * Can be used like an array and often describes a point or
  * distances in space. Mathematical operations can be performed
  * with vectors and matrices, which makes it possible to rotate
- * transform or translate the defined points in 3D perspective.
+ * transform or translate defined points in 3D perspective.
+ * @ingroup util
  */
-/// @ingroup util
-class util::Vector
+class Vector2
 {
 public:
-	Vector();
-	~Vector();
+	float x; ///< x value of Vector
+	float y; ///< y value of Vector
+
+	Vector2(); ///< Default constructor
+	Vector2(const Vector2 &vec); ///< Copy constructor
+	Vector2(const float vx, const float vy); ///< Copy constructor
+	~Vector2(); ///< Destructor
+
+	void Set(const float vx, const float vy); ///< Sets values of Vector
+	bool Compare(const Vector2 &vec) const; ///< Compares with Vector
+	float Length() const; ///< Returns length of Vector
+
+	float operator[](int index) const; ///< Subscript operator for constant
+	float &operator[](int index); ///< Subscript operator for variable
+	void operator=(const Vector2 &vec); ///< Assignment operator
+	Vector2 &operator+=(const Vector2 &vec); ///< Addition compound operator
+	Vector2 &operator-=(const Vector2 &vec); ///< Subtraction compound operator
+
+	// NOTE: also using these non-member, non-friend functions:
+	// bool operator==(const Vector2 &a, const Vector2 &b);
+	// bool operator!=(const Vector2 &a, const Vector2 &b);
+	// Vector2 operator+(const Vector2 &a, const Vector2 &b);
+	// Vector2 operator-(const Vector2 &a, const Vector2 &b);
 };
+
+} // namespace util
