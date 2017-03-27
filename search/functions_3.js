7851395f51ccc6e1ae2281d2e17e07a542cec078
Travis CI <travis@travis-ci.org>
2017-03-27 12:39:10 +0000

    Deploy docs for commit 1050db99d6113f09ca17bc95890e5035739eb30e

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['init',['Init',['../classutil_1_1String.html#abe05cfbfb31ab7703efe73e8c59d3113',1,'util::String']]],
-  ['invsqrt',['InvSqrt',['../namespaceutil.html#a03949d3d2ce130c1914a83e6501d4e35',1,'util']]]
+  ['init',['Init',['../classutil_1_1String.html#abe05cfbfb31ab7703efe73e8c59d3113',1,'util::String']]]
 ];
